f61b3d94842eb04469d075d5662518ce3c479a26
subroh0508 <subroh.0508@gmail.com>
2018-12-15 22:34:02 +0900

    [edit]Set port 3000

--- server/index.py
+++ server/index.py
@@ -5,7 +5,7 @@ api = responder.API()
 async def home(request, response):
     body = await request.media()
 
-    response.media = { 'url': body['tweet'] }
+    response.media = { 'url': 'https://4.bp.blogspot.com/-DcD4rBwJpK4/WlGpoy3pIBI/AAAAAAABJo0/h_bt-BS3gM4DxnZO3Q6rOim1FPJE83NMACLcBGAs/s180-c/sweets_potatochips_chocolate.png' }
 
 if __name__ == '__main__':
-    api.run()
+    api.run(port = 3000)
