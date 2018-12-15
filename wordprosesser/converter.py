1fefcaa6fc2fcce70eea9f44ae4820345effce08
tks10 <tktktks10@gmail.com>
2018-12-15 23:35:21 +0900

    uodate: embed

--- wordprosesser/converter.py
+++ wordprosesser/converter.py
@@ -1,5 +1,8 @@
 import pickle
 import MeCab
+import sys, os
+from wordprosesser.embedder import load_model
+
 
 def load(pickle_file="../irasutoya_contents.pickle"):
     with open(pickle_file, 'rb') as f:
@@ -20,6 +23,7 @@ def pickle_to_text(src, dst):
 
     with open(dst, mode='w') as f:
         f.write('\n'.join(lines))
+
 
 def separate(text):
     mecab = MeCab.Tagger("-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
@@ -42,8 +46,36 @@ def separate(text):
     return sentence_processed
 
 
+def dict_to_vector(src):
+    with open(src) as f:
+        lines = list(map(lambda l: l.strip(), f.readlines()))
+
+    model = load_model("../model/entity_vector.model.bin")
+
+    lines = lines[:100]
+    outputs = []
+
+    for line in lines:
+        sentence, url = line.split(",")
+        words = sentence.split()
+        vectors = []
+        for word in words:
+            try:
+                vector = model[word]
+                vectors.append(vector)
+            except KeyError:
+                pass
+
+        line_vectored = {"vectors": vectors, "url": url}
+        outputs.append(line_vectored)
+
+    with open('../res/irasutoya_vectors.pickle', 'wb') as f:
+        pickle.dump(outputs, f)
+
+
 def main():
-    pickle_to_text("../res/irasutoya_contents.pickle", "../res/irasutoya_dict.txt")
+    # pickle_to_text("../res/irasutoya_contents.pickle", "../res/irasutoya_dict.txt")
+    dict_to_vector("../res/irasutoya_dict.txt")
 
 
 if __name__ == "__main__":
