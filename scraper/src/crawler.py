ab169956b542ab99f8b1d623d2edc16f0486d9f8
tks10 <tktktks10@gmail.com>
2018-12-15 13:51:08 +0900

    init class

---
+++ scraper/src/crawler.py
@@ -0,0 +1,22 @@
+import json
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
+import codecs
+from time import sleep
+from joblib import Parallel, delayed
+from collections import deque
+import argparse
+
+
+class Clawler:
+    def __init__(self):
+        pass
+
+
+def test():
+    pass
+
+
+if __name__ == '__main__':
+    test()
