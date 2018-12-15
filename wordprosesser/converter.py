48a2439a9d39fc719f942fa54f4bd7970f07732b
subroh0508 <subroh.0508@gmail.com>
2018-12-15 22:13:26 +0900

    [add]Add irasutoya_dict.txt

---
+++ wordprosesser/converter.py
@@ -0,0 +1,50 @@
+import pickle
+import MeCab
+
+def load(pickle_file="../irasutoya_contents.pickle"):
+    with open(pickle_file, 'rb') as f:
+        result = pickle.load(f)
+
+    print(len(result))
+
+
+def pickle_to_text(src, dst):
+    with open(src, 'rb') as f:
+        result = pickle.load(f)
+
+    lines = []
+    for r in result:
+        title_processed = separate(r["name"])
+        url = r["url"]
+        lines.append(title_processed + "," + url)
+
+    with open(dst, mode='w') as f:
+        f.write('\n'.join(lines))
+
+def separate(text):
+    mecab = MeCab.Tagger("-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd")
+    mecab.parse('')
+
+    stop_pos = ["助詞", "助動詞", "BOS/EOS"]
+    stop_words = ["イラスト", "（", "）", "*"]
+
+    node = mecab.parseToNode(text)
+
+    word_filtered = []
+    while node:
+        word = node.feature.split(",")[6]
+        pos = node.feature.split(",")[0]
+        if not (pos in stop_pos or word in stop_words):
+            word_filtered.append(word)
+        node = node.next
+    sentence_processed = " ".join(word_filtered)
+
+    return sentence_processed
+
+
+def main():
+    pickle_to_text("../res/irasutoya_contents.pickle", "../res/irasutoya_dict.txt")
+
+
+if __name__ == "__main__":
+    main()
